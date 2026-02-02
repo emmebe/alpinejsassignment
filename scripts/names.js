3ac4f55ff5ab663470aff5f086024dd1a290afb8
Emma <ebelinge@msu.edu>
2026-02-02 03:39:56 +0000

    created name list, created random name func

---
+++ scripts/names.js
@@ -0,0 +1,16 @@
+const listNames = [
+    "Emma",
+    "Audrey",
+    "Cierra",
+    "Allie",
+    "Peyton",
+    "Molly",
+    "Ike",
+    "Thing 1",
+    "Thing 2",
+    "Last Name"
+];
+
+function getRandomName(max) {
+    return Math.floor(Math.random() * max);
+}
